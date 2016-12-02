c602b4f4bc39e545507c40b03911dde5544afd6f
klramos <klramos@ncsu.edu>
2016-12-02 14:47:31 -0500

    updated .gitignore

--- callbacks/set_available_constraintbox.m
+++ callbacks/set_available_constraintbox.m
@@ -51,8 +51,9 @@ try
     handles.guidata.coeff{cp} = handles.xrd.getCoeff(fcnNames, handles.guidata.constraints{cp});
 catch ME
     
-   handles.guidata.coeff{cp} = [];
+   
    [info,index] = dbstack('-completenames', 1);
    uiwait(errordlg(['Error in ' info(1).file ' line ' num2str(info(1).line) ': ' ME.message]))
+   handles.guidata.coeff{cp} = [];
    
 end
