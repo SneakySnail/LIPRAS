60b2af30622321269fd93589a53f07513bf32a99
SneakySnail <gesteves21@gmail.com>
2016-12-21 16:24:58 -0500

    Fixing the read algorithm for csv and XRDML files

--- @PackageFitDiffractionData/private/parseXRDML.m
+++ @PackageFitDiffractionData/private/parseXRDML.m
@@ -1,4 +1,4 @@
-function parseXRDML(Stro)
+function parseXRDML(Stro,index)
 	
 	Stro.suffix = 'xrdml';
 	
@@ -9,8 +9,8 @@ function parseXRDML(Stro)
 	Z = 0;
 	fileIndex = 0;
 	
-	for fileNumb = 1:length(Stro.Filename)
-		file = Stro.Filename{fileNumb};
+  fileNumb=index;  
+		file = strcat(Stro.DataPath,Stro.Filename{fileNumb});
 		[parthstr,name,ext] = fileparts(file);
 		if strcmp(ext, char('.xrdml'))
 			data = parseXML(file);
@@ -24,6 +24,8 @@ function parseXRDML(Stro)
 					tth = 0;
 					fileIndex = fileIndex + 1;
 					scanIndex = i;
+                   
+                    
 					for PosI = 1:length(data.Children(1,dataIndex).Children(1,scanIndex).Children)
 						if strcmp(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Name, 'dataPoints')
 							for DataPointsi = 1:length(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Children)
@@ -36,17 +38,18 @@ function parseXRDML(Stro)
 											tthf = strread(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Children(1,DataPointsi).Children(1,4).Children(1,1).Data,'%f');
 										end
 									end
-								end
-								if strcmp(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Children(1,DataPointsi).Name, 'intensities')
+                                end
+                                    if strcmp(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Children(1,DataPointsi).Name, 'intensities')
 									intensity = strread(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Children(1,DataPointsi).Children(1,1).Data,'%f');
-								end
+                                    end
 							end
 						end
 						if strcmp(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Name, 'nonAmbientPoints')
-							temperature = mean(strread(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Children(1,4).Children(1,1).Data,'%f'))-273.15;
+							temperature(PosI,:) = mean(strread(data.Children(1,dataIndex).Children(1,scanIndex).Children(1,PosI).Children(1,4).Children(1,1).Data,'%f'))-273.15;
 						else
-							temperature = 25;
+							temperature(PosI,:) = 25;
 						end
+                        
 					end
 					if tth == 0
 						step = (tthf - ttho) / (length( intensity )-1);
@@ -57,45 +60,48 @@ function parseXRDML(Stro)
 							Data{DataIndex,1} = 0;
 							fileIndex = 1;
 						end
-					end
-					if Data{DataIndex,1} == 0
-						Data{DataIndex,1} = cat(Data{DataIndex,1},tth');
-						Data{DataIndex,2}(1:length(tth)) = temperature;
-						Data{DataIndex,3} = intensity';
-					else
-						Data{DataIndex,1}(fileIndex,:) = tth';
-						Data{DataIndex,2}(fileIndex,:) = temperature;
-						Data{DataIndex,3}(fileIndex,:) = intensity';
-					end
+                    end
+                    
+
+
 				end
-			end
-		end
-	end
+            end
+        end
+        
+        % Reading Kalpha1, Kalpha2, Kbeta, and Ratio from XRDML
+                    % how to read XML, if the element is tabbed over twice,
+                    % you need two instances of Children to access it then
+                    % the Name, or Attribute. To read the value within it,
+                    % you will need another Children since it will be
+                    % tabbed over again.
+                    for p=1:scanIndex
+                                if strcmp(data.Children(1,dataIndex).Children(1,p).Name, 'usedWavelength')
+                                            KAlpha1=data.Children(1,dataIndex).Children(1,4).Children(1,2).Children(1,1).Data;
+                                            KAlpha2=data.Children(1,dataIndex).Children(1,4).Children(1,4).Children(1,1).Data;
+                                            kBeta=data.Children(1,dataIndex).Children(1,4).Children(1,6).Children(1,1).Data;
+                                            Ratio_alph1_alph2=data.Children(1,dataIndex).Children(1,4).Children(1,8).Children(1,1).Data;
+                                    disp('1')
+                                end
+                        
+                    end       
+        
+        
+
+	    
+    
 	
-	for k = 2:DataIndex
-		X = Data{k,1};
-		Y = Data{k,2};
-		Z = Data{k,3};
-		numScans = size(X);
-		numScans = numScans(1);
-		for i = 1:numScans
-			for j = i:numScans
-				if Y(j,1) < Y(i,1)
-					swapZ = Z(j,:);
-					swapY = Y(j,:);
-					Y(j,:) = Y(i,:);
-					Z(j,:) = Z(i,:);
-					Y(i,:) = swapY;
-					Z(i,:) = swapZ;
-				end
-			end
-		end
-		Data{k,1} = X;
-		Data{k,2} = Y;
-		Data{k,3} = Z;
-	end
-	
-	Stro.two_theta = Data{2,1}(1,:);
-	Stro.data_fit = Data{2:DataIndex,3};
-	Stro.temperature = Data{2:DataIndex,2}(:,1);
+    Stro.KAlpha1(index,:)=str2double(KAlpha1);
+    Stro.KAlpha2(index,:)=str2double(KAlpha2);
+    Stro.KBeta(index,:)=str2double(kBeta);
+    Stro.RKa1Ka2(index,:)=str2double(Ratio_alph1_alph2);
+    
+	Stro.two_theta = tth';
+	Stro.data_fit(index,:) = intensity';
+    if length(unique(temperature))==2
+        temperature=unique(temperature);
+        temperature=temperature(2);
+    else
+        temperature=25;
+    end
+	Stro.Temperature(index,:) = temperature;
 end
--- @PackageFitDiffractionData/private/readFile.m
+++ @PackageFitDiffractionData/private/readFile.m
@@ -4,8 +4,21 @@ for i=1:Stro.skiplines
 		fgetl(fid);
 end
 if or(strcmp( Stro.suffix, 'csv'), strcmp( Stro.suffix, '.csv'))
-		datain = xlsread(fid,'A:B');
-		datain = transpose(datain);
+		datain = xlsread(fid);
+        % Method for reading of data that does not start with numerial
+        % twotheta and intensity
+        cc=isnan(datain(:,1)); % checks if any NaN were read in
+            for i=1:length(cc) %  sums the results of cc if after summing 5 rows and the sum is 0, then it re-shapes the data read in with xlsread
+                s= sum(cc(i:i+5),1);
+                    if s==0
+                        p=i;
+                        break
+                    end
+            end
+
+        datain=datain(p:end,:); % reshapes based on for loop results
+        datain=datain(:,1:2)'; % now takes the first two columns of intesity and 2-theta and transpose     
+        
 elseif or(strcmp( Stro.suffix, 'xy'), strcmp( Stro.suffix, '.xy'))
 		datain = fscanf(fid,'%f',[2 ,inf]);
 elseif or(strcmp( Stro.suffix, 'fxye'), strcmp( Stro.suffix, '.fxye'))
@@ -18,7 +31,10 @@ else
 		datain = fscanf(fid,'%f',[2 ,inf]);
 end
 
-fclose(fid);
+if or(strcmp( Stro.suffix, 'csv'), strcmp( Stro.suffix, '.csv'))
+else
+fclose(fid); % xlsread does not need fclose
+end
 
 Stro.two_theta = datain(1,:);
 Stro.data_fit(index,:) = datain(2,:);
