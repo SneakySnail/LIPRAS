4732732ad1bbda3ffdcdfbb6dec0b76f70e87e0e
klramos <klramos@ncsu.edu>
2017-01-04 01:14:06 -0500

    Inverted the display of column and row names in table_results back to the original display of the coefficients being listed as rows instead of columns

--- callbacks/fill_table_results.m
+++ callbacks/fill_table_results.m
@@ -1,18 +1,21 @@
 function fill_table_results(handles)
-	try
-		set(handles.table_results, ...
-            'ColumnName',handles.xrd.Fcoeff{1}, ...
-            'Data',cell(length(handles.xrd.Filename),length(handles.xrd.Fcoeff{1})), ...
-            'RowName', handles.xrd.Filename);
-		
-		assert(length(handles.xrd.Filename)==length(handles.xrd.fit_parms));
-		
-		for i=1:length(handles.xrd.Filename)
-			assert(length(handles.table_results.Data(i,:))==length(handles.xrd.fit_parms{i}));
-			handles.table_results.Data(i,:) = num2cell(handles.xrd.fit_parms{i});
-		end
-		
-    catch
-        dbstack
-	end
-	
+try
+    result_vals = transpose(vertcat(handles.xrd.fit_parms{:}));
+
+    coeff = handles.cfit(1).Coefficients;
+    set(handles.table_results, ...
+        'Data', num2cell(result_vals), ...
+        'RowName', coeff, ...
+        'ColumnName', handles.xrd.Filename, ...
+        'ColumnFormat', {'numeric'}, ...
+        'ColumnWidth', {'auto'}, ...
+        'ColumnEditable', false);
+    
+    for i=1:length(handles.xrd.Filename)
+%         assert(length(handles.table_results.Data(i,:))==length(handles.xrd.fit_parms{i}));
+        handles.table_results.Data(i,:) = num2cell(handles.xrd.fit_parms{i});
+    end
+    
+catch ME
+        keyboard
+end
