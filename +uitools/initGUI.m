7b3c33ae1245b93d4a15d09acc68d43560cc9a7a
klramos <klramos@ncsu.edu>
2017-01-06 12:36:16 -0500

    Changed background color of status bar to white

--- +uitools/initGUI.m
+++ +uitools/initGUI.m
@@ -1,138 +1,149 @@
 % Initialize GUI controls
-function handles = initGUI(handles)    
-    addToExecPath();
-    
-    initComponents();
-    
-    createJavaStatusBar();
-    
-    createUserData();
-    
-    handles = resetGuiData(handles);
-    
-    addControlListeners();
+function handles = initGUI(handles)
+addToExecPath();
 
-    reparentTabPanels();
-    
-%      handles.edit_polyorder = uitools.uispinner(handles.edit_polyorder, 3, 1, 25, 1);
-    
-    addLastCallbacks();
+initComponents();
+
+createJavaStatusBar();
+
+createUserData();
+
+handles = resetGuiData(handles);
+
+addControlListeners();
+
+reparentTabPanels();
+
+addLastCallbacks();
 
 % ==============================================================================
-    
-    
-    %% helper functions
+
+
+%% helper functions
     function addToExecPath()
-        addpath(genpath('callbacks'));
-        addpath(genpath('dialog'));
-        addpath(genpath('listener'));
-        % addpath(genpath('Resources'));
-        % addpath('test-path/');
+    addpath(genpath('callbacks'));
+    addpath(genpath('dialog'));
+    addpath(genpath('listener'));
+    % addpath(genpath('Resources'));
+    % addpath('test-path/');
     end
-    % ==========================================================================
-    
+% ==========================================================================
+
     function initComponents()
-        hold(handles.axes1, 'on');
-        
-        % Default color order for plotting data series
-        set(get(handles.axes1, 'Parent'), 'DefaultAxesColorOrder', ...
-            [0 0 0; % black
-            1 0 0; % red
-            1 0.4 0; % orange
-            0.2 0.2 0; % olive green
-            0 0 0.502; % navy blue
-            0.502 0 0.502; % violet
-            0 0 1; % royal blue
-            0.502 0.502 0]); % dark yellow
-        
-        set(handles.table_paramselection, 'Data', cell(1, 1), ...
-            'enable', 'on', 'ColumnName', {'Peak function'}, ...
-            'ColumnWidth', {250}, 'Data', cell(1, 1));
-        
-    end
-    % ==========================================================================
+    hold(handles.axes1, 'on');
     
+    % Default color order for plotting data series
+    set(get(handles.axes1, 'Parent'), 'DefaultAxesColorOrder', ...
+        [0 0 0; % black
+        1 0 0; % red
+        1 0.4 0; % orange
+        0.2 0.2 0; % olive green
+        0 0 0.502; % navy blue
+        0.502 0 0.502; % violet
+        0 0 1; % royal blue
+        0.502 0.502 0]); % dark yellow
+    
+    set(handles.table_paramselection, 'Data', cell(1, 1), ...
+        'enable', 'on', 'ColumnName', {'Peak function'}, ...
+        'ColumnWidth', {250}, 'Data', cell(1, 1));
+    
+    o = handles.toolbar_legend;
+    LIPRAS('toolbar_legend_ClickedCallback', o, [], guidata(o));
+    
+    end
+% ==========================================================================
+
     function createUserData()
-        handles.profiles(7) = handles.uipanel3;
-        handles.profiles(7).UserData = 0; % delete
-        handles.xrd = PackageFitDiffractionData;
-        handles.xrdContainer(7) = handles.xrd;
-        
-    end
-    % ==========================================================================
+    handles.profiles(7) = handles.uipanel3;
+    handles.profiles(7).UserData = 0; % delete
+    handles.xrd = PackageFitDiffractionData;
+    handles.xrdContainer(7) = handles.xrd;
     
+    end
+% ==========================================================================
+
     function addControlListeners()
-        addlistener(handles.xrdContainer(7), 'Status', ...
-            'PostSet', @(o,e)statusChange(o,e,handles,7));
-        
-        addlistener(handles.axes1, 'ColorOrderIndex', ...
-            'PostSet', @(o,e)colorOrderIndexChanged(o,e,guidata(e.AffectedObject)));
-    end
-    % ==========================================================================
+    addlistener(handles.xrdContainer(7), 'Status', ...
+        'PostSet', @(o,e)statusChange(o,e,handles,7));
     
-    % Creates the Java status bar, used for updating the user on GUI actions. Throws
-    % an exception if the Java object could not be created.
+    addlistener(handles.axes1, 'ColorOrderIndex', ...
+        'PostSet', @(o,e)colorOrderIndexChanged(o,e,guidata(e.AffectedObject)));
+    end
+% ==========================================================================
+
+% Creates the Java status bar, used for updating the user on GUI actions. Throws
+% an exception if the Java object could not be created.
     function createJavaStatusBar()
+    % When calling for the underlying java window, it will return empty unless
+    % the figure is visible
+    import javax.swing.border.EtchedBorder
+    import javax.swing.BorderFactory
+    import java.awt.BorderLayout
+    import java.awt.Color
+    
+    set(handles.figure1, 'visible', 'on'); 
+    try
+        % Turn off JavaFrame obsolete warning
+        warning off MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame;
+        jFrame=get(handles.figure1,'JavaFrame');
         try
-            % Turn off JavaFrame obsolete warning
-            warning off MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame;
-            jFrame=get(handles.figure1,'JavaFrame');
-            try
-                jRootPane = jFrame.fFigureClient.getWindow;  % This works up to R2011a
-            catch
-                try
-                    jRootPane = jFrame.fHG1Client.getWindow;  % This works from R2008b-R2014a
-                catch
-                    jRootPane = jFrame.fHG2Client.getWindow;  % This works from R2014b and up
-                end
-            end
-            
-            
-            % left status bar
-            handles.statusbarObj = javaObjectEDT('com.mathworks.mwswing.MJStatusBar');
-            jRootPane.setStatusBar(handles.statusbarObj);
-            handles.statusbarObj.setText('<html>Please import file(s) containing data to fit.</html>');
-            
-            % separator
-%             sep = javaObjectEDT('javax.swing.JSeparator');
-%             sep
-            % right status bar
-            handles.statusbarRight = javaObjectEDT('com.mathworks.mwswing.MJStatusBar');
-            handles.statusbarObj.add(handles.statusbarRight, 'East');
-            handles.statusbarRight.setText('');
-            jRootPane.setStatusBarVisible(1);
-            
+            jRootPane = jFrame.fFigureClient.getWindow;  % This works up to R2011a
         catch
-            msgId = 'initGUI:JavaObjectCreation';
-            msg = 'Could not create the Java status bar';
-            MException(msgId, msg);
+            try
+                jRootPane = jFrame.fHG1Client.getWindow;  % This works from R2008b-R2014a
+            catch
+                jRootPane = jFrame.fHG2Client.getWindow;  % This works from R2014b and up
+            end
         end
+        
+        
+        % left status bar
+        handles.statusbarObj = javaObjectEDT('com.mathworks.mwswing.MJStatusBar');
+        handles.statusbarObj = javacomponent(handles.statusbarObj, 'South');
+        handles.statusbarObj = handles.statusbarObj.getComponent(0);
+        handles.statusbarObj.setBackground(Color.white);
+%         jRootPane.setStatusBar(handles.statusbarObj);
+        handles.statusbarObj.setText('To start, import file(s) from your computer to fit.');
+        
+        % border
+        %             newBorder = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+        %             newBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+        %             newBorder = BorderFactory.createLoweredBevelBorder();
+        %         newBorder = BorderFactory.createRaisedBevelBorder();
+%         newBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+%         handles.statusbarObj.setBorder(newBorder);
+        
+    catch
+        msgId = 'initGUI:JavaObjectCreation';
+        msg = 'Could not create the Java status bar';
+        MException(msgId, msg);
     end
-    % ==========================================================================
-    
-    % Set the parents of the 3 major panels for tab switching functionality.
+    end
+% ==========================================================================
+
+% Set the parents of the 3 major panels for tab switching functionality.
     function reparentTabPanels()
-        set(handles.panel_setup, 'parent', handles.profiles(7));
-        set(handles.panel_parameters,'parent', handles.profiles(7));
-        set(handles.panel_results, 'parent', handles.profiles(7)); 
+    set(handles.panel_setup, 'parent', handles.profiles(7));
+    set(handles.panel_parameters,'parent', handles.profiles(7));
+    set(handles.panel_results, 'parent', handles.profiles(7));
     end
-    % ==========================================================================
-        
-    
-    % Adds callback functions to all other uicomponents. 
-    % 
-    % Assumes this is the last function called in the GUI initialization.
-    % 
-    % Throws an exception if the status bar is invalid.
+% ==========================================================================
+
+
+% Adds callback functions to all other uicomponents.
+%
+% Assumes this is the last function called in the GUI initialization.
+%
+% Throws an exception if the status bar is invalid.
     function addLastCallbacks()
-        
-        % Requires a Java status bar to exist
-        if ~isa(handles.statusbarObj, 'com.mathworks.mwswing.MJStatusBar')
-            msgId = 'initGUI:InvalidJavaStatusBar';
-            msg = 'Could not add a callback function for updating the status bar.';
-            MException(msgId, msg);
-        end
-        handles.figure1.WindowButtonMotionFcn = @(o, e)WindowButtonMotionFcn(o, e,guidata(o));
+    
+    % Requires a Java status bar to exist
+    if ~isa(handles.statusbarObj, 'com.mathworks.mwswing.MJStatusBar')
+        msgId = 'initGUI:InvalidJavaStatusBar';
+        msg = 'Could not add a callback function for updating the status bar.';
+        MException(msgId, msg);
     end
-    % ==========================================================================
+    handles.figure1.WindowButtonMotionFcn = @(o, e)WindowButtonMotionFcn(o, e,guidata(o));
+    end
+% ==========================================================================
 end
