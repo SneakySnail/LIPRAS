a5e6480578ec0a34e4a8d6111188483e264844a8
SneakySnail <gesteves21@gmail.com>
2017-02-25 21:20:52 -0500

    adding them all, this is what happens when you store multiple versions in different folders/computers, this version should work

--- +ui/+control/@GUIController/GUIController.m
+++ +ui/+control/@GUIController/GUIController.m
@@ -141,15 +141,20 @@ classdef GUIController < handle
         end
         if this.Profiles.hasFit
             fitted = this.Profiles.getProfileResult{1};
-            if ~isequal(fitted.FunctionNames, this.FcnNames) || ...
+             if ~isequal(fitted.FunctionNames, this.FcnNames) || ...
                     ~isequal(fitted.CoeffNames, this.Coefficients) || ...
                     ~isequal(fitted.FitInitial, this.FitInitial)
                 result = true;
-            end
-        elseif isequal(this.FcnNames, xrd.getFunctionNames) && ...
+            elseif isequal(this.FcnNames, xrd.getFunctionNames) && ... % this elseif is to yield a result=false after completing a fit
                 isequal(this.Coefficients, xrd.getCoeffs) && ...
                 isequal(this.FitInitial, xrd.FitInitial)
             result = false;
+                
+            elseif isequal(this.FcnNames, xrd.getFunctionNames) || ... % this else if is to yield result=true when a constaint has been checked
+                isequal(this.Coefficients, xrd.getCoeffs) || ...
+                isequal(this.FitInitial, xrd.FitInitial)
+            result = true;
+            end
         else
             result = true;
         end
