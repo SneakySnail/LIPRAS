efa9e6bda9169f4149a0f3c3f07e856cbde0d031
Giovanni Esteves <gesteves21@gmail.com>
2017-05-26 16:47:36 -0400

    Match commit number

--- +model/ProfileListManager.m
+++ +model/ProfileListManager.m
@@ -5,7 +5,7 @@ classdef ProfileListManager < handle & matlab.mixin.SetGet
     %   ProfileListManager.getInstance.
    properties
        
-       LIPRAS_Version=412;
+       LIPRAS_Version=414;
        DataPath = [];
        
        OutputPath = ['FitOutputs' filesep];
