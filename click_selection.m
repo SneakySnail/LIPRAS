20b6acd6b7d8dd7b8553cb9ccbbac8da35577596
G10 <gesteves21@gmail.com>
2026-01-05 21:36:45 -0700

    Bug changes and deleted odd files

--- click_selection.m
+++
@@ -1,5 +0,0 @@
-            function click_selection(app, event)
-            temp = app.UIAxes.CurrentPoint; % Returns 2x3 array of points
-            loc = [temp(1,1) temp(1,2)]; % Gets the (x,y) coordinates
-            disp(loc)
-            end  
